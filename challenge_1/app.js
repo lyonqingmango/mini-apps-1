9f3b425905c546e690d32da3675e12ca58e86d43
lyonqingmango <qingmango@gmail.com>
2022-05-18 21:07:12 -0700

    add event listern

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,53 @@
-// const para = document.querySelector('p');
 
-// para.addEventListener('click', updateName);
+var player="X";
+var board =["","","",
+            "","","",
+            "","",""]
 
-// function updateName() {
-//   let name = prompt('输入一个新的名字：');
-//   para.textContent = '玩家1：' + name;
-// }
-console.log('hello')
+
+
+
+  var box= document.getElementById("0");
+  function updateBox(event){
+    var index= event.target.id;
+    var i= parseInt(index);
+
+    if(board[i].length === 0){
+      if(player ==='X'){
+        event.target.innerHTML ='X';
+        player ='O';
+        board[i]='X';
+
+      }else {
+        event.target.innerHTML ='O';
+        player ='X';
+      }
+
+    }
+
+
+}
+box.addEventListener("click", updateBox,false);
+
+
+
+
+
+
+
+
+
+function reset(){
+  var boxes = document.querySelectorAll(".box");
+  boxes.forEach(function(box){box.innerHTML =""});
+
+}
+
+var button = document.querySelector(".button");
+console.log(button);
+button.addEventListener("click", reset);
+
+
+// console.log('hello')
 
 
