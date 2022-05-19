8470b6bf46be36aef0a8e59e82867fe376e79e66
lyonqingmango <qingmango@gmail.com>
2022-05-18 18:28:24 -0700

    layout

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
+// const para = document.querySelector('p');
+
+// para.addEventListener('click', updateName);
+
+// function updateName() {
+//   let name = prompt('输入一个新的名字：');
+//   para.textContent = '玩家1：' + name;
+// }
+console.log('hello')
 
 
