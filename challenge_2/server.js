41223baaf1531c03a4688ea0692cdc884e05b320
lyonqingmango <qingmango@gmail.com>
2022-05-21 12:53:10 -0700

    working on Server Responses based on Form Submission

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({extended: false}));
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static("client"));
+
+
+
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+});
+app.post('/upload_json',(req, res) => {
+
+  console.log('requestbodydata12_____'+ req.body.data);
+  var obj = req.body.data;
+  var objKeys =Object.keys(JSON.parse(obj));// Json convert to obj
+  console.log(objKeys);
+
+
+
+
+
+
+
+
+
+  res.end('post')
+
+
+ })
+
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+});
 
 
