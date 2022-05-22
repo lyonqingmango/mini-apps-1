9cc6c2e93a9ad2aaab2b7c40b46f1c70f120a1e1
lyonqingmango <qingmango@gmail.com>
2022-05-22 15:20:17 -0700

    complete bmr

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,15 +1,51 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World!')
-// });
 
-// app.listen(port, function() {
-//   console.log(`Example app listening on port ${port}!`)
-// });
-console.log('hello')
+$(document).ready(function() {
+
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    console.log('submit');
+    console.log('form'+ $('form').serialize());
+
+    // var formData = new FormData(myForm);
+    // console.log(formData)
+
+    $.ajax({
+            url: '/upload_json',
+            type: 'POST',
+            data: $('form').serialize(),
+            contentType: false,
+            processData: false,
+            contentType: 'application/x-www-form-urlencoded',
+
+            success: function(result){
+              console.log('result'+result);
+              var blob=new Blob([result], {type: 'text/csv'});
+              var link=document.createElement('a');
+
+              link.style = "display: none";
+              document.body.appendChild(link);
+
+              var url = URL.createObjectURL(blob);
+              link.href = url;
+              console.log(url);
+              link.download="download.csv";
+              link.click();
+              URL.revokeObjectURL(url);
+          },
+          error: function(err){
+            console.log('err'+err);
+          },
+
+    });
+  });
+
+})
+
+
+
+
+
 
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,11 +3,43 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static("client"));
 
+var convertToCSV = (obj) => {
+  var text=''
+   var keys =Object.keys(obj);
+   keys.pop();
+   text+= keys.join();
+   text+= "\n";
+   console.log(text)
 
+   var helper = function (obj){
+     for(var key in obj){
+       if(key!=='children'){
+         text+=obj[key];
+         text+=','
+       }else{
+         text=text.slice(0,-1);
+         text+='\n'
+         if(obj.children.length!==0){
+           for(let i=0;i<obj.children.length;i++){
+               helper(obj.children[i]);
+           }
+           return text
+         }else{
+           return text;
+         }
+       }
+     }
+     return text;
+   }
+   text =helper(obj);
+   return text
+
+  //  console.log('text==='+helper(obj))
+ }
 
 
 
@@ -17,21 +49,10 @@ app.get('/', function(req, res) {
 app.post('/upload_json',(req, res) => {
 
   console.log('requestbodydata12_____'+ req.body.data);
-  var obj = req.body.data;
-  var objKeys =Object.keys(JSON.parse(obj));// Json convert to obj
-  console.log(objKeys);
-
-
-
-
-
-
-
-
-
-  res.end('post')
-
-
+  var obj = JSON.parse(req.body.data);
+  var result = convertToCSV(obj)
+  console.log(result);
+  res.send(result);
  })
 
 
